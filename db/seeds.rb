1ab14d155e1c4671d2c6ae797416684a86678cb5
Scott Geryol <sgeryol@gmail.com>
2020-08-10 21:07:39 -0500

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,8 @@ Beer.create(name: "Parrot of Paradise", brewery: "Half Acre Beer Company", style
 Beer.create(name: "Lager", brewery: "Dovetail Brewery", style: "Lager", abv: "4.7%", rating: 8 / 10)
 Beer.create(name: "Broccoli", brewery: "Other Half Beer Company", style: "IPA", abv: "7.9%", rating: 8 / 10)
 
-Brewery.create(name: "Half Acre Beer Company", location: "Chicago, IL", beer_id: 1, rating: 9, website: "https://www.halfacrebeer.com/)"
-Brewery.create(name: "Dovetail Brewery", location: "Chicago, IL", beer_id: 1, rating: 8, website: ""
-Brewery.create(name: "Other Half Beer Comnpany", location: "Brooklyn, NY", beer_id: 1, rating: 8, website: ""
+Brewery.create(name: "Half Acre Beer Company", location: "Chicago, IL", beer_id: 1, rating: 9, website: "https://www.halfacrebeer.com/)")
+Brewery.create(name: "Dovetail Brewery", location: "Chicago, IL", beer_id: 1, rating: 8, website: "")
+Brewery.create(name: "Other Half Beer Comnpany", location: "Brooklyn, NY", beer_id: 1, rating: 8, website: "")
+
+puts "yes"
