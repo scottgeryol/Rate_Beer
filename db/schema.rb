ae35f4e2ac1f5e726371f065b17a64362919b518
Scott Geryol <sgeryol@gmail.com>
2020-08-10 21:05:19 -0500

    migrated brewery model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_234718) do
+ActiveRecord::Schema.define(version: 2020_08_11_015431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 2020_07_24_234718) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "breweries", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "beer_id"
+    t.integer "rating"
+    t.string "website"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -32,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_07_24_234718) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 end
