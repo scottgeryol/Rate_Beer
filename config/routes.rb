00da797f259e80249e2fd6e1a4826e2fbe2853ce
Scott Geryol <sgeryol@gmail.com>
2020-08-10 20:35:33 -0500

    fixed beer controller route bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/beers" => "beer#index"
-    post "/beers" => "beers#create"
-    get "/beers/:id" => "beers#show"
+    post "/beers" => "beer#create"
+    get "/beers/:id" => "beer#show"
   end
 end
