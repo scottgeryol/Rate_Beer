4d290c83e84217f0b0d0033da0968e1f5ea2b6b7
Scott Geryol <sgeryol@gmail.com>
2020-08-26 20:56:41 -0500

    added sessions route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
 
     get "/beers" => "beer#index"
     post "/beers" => "beer#create"
